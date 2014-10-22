072c2f35ca5ba5a37c96a473665df1bf41195220
Emily Bamsey <em_bamsey@laurentian.ca>
2014-10-22 09:54:01 -0400

    Updated the update script
    It no longer needs to be in the import folder, just in the same location
    as the import folder.

--- updateWinterhalder.sh
+++ updateWinterhalder.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
-# loop through each item and change the file in the contents folder to be the master
+# This script should be run in the same directory as the winterhalder_imports directory
 
-for folders in ./*; do
-	sed -i s/t.jpg/m.jpg/g $folders/contents
+# loop through each item and change the image file in the contents folder to be the master
+
+for folders in ./winterhalder_imprts/*; do
+	# Ensure we are only running the find and replace on the item folders
+	if [ -d "$folders" ] ; then
+		sed -i s/t.jpg/m.jpg/g $folders/contents
+	fi
 done 
